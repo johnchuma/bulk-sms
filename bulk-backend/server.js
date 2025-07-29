f3e763ae61bef4745274b9af9a20a38c01b0d875
johnchuma <johnvchuma@gmail.com>
2025-07-29 11:19:13 +0300

    fix: server

--- bulk-backend/server.js
+++ bulk-backend/server.js
@@ -12,7 +12,7 @@ const clientRoutes = require("./routes/clientRoutes");
 const authRoutes = require("./routes/authRoutes");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5005;
 
 // Middleware
 app.use(helmet());
